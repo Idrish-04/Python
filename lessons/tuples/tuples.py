a086a34373fec046282f4b95db9df5f983ae0d24
idrish <ikatwara2411@gmail.com>
2026-06-04 22:39:54 +0530

    commit

---
+++ lessons/tuples/tuples.py
@@ -0,0 +1,10 @@
+t=tuple()
+print(t)
+print(type(t))
+fruits=("mango","banana","apple","lemon" )
+f= len(fruits)
+print(f)
+first=fruits[0]
+print(first)
+last_index=len(fruits)-1
+print(last_index)
---
+++ lessons/tuples/tuplestolist.py
@@ -0,0 +1,9 @@
+fruits=('banana', 'orange', 'mango', 'lemon')
+lis=list(fruits)
+print(lis)
+orange_mango = fruits[-3:-1]  
+print(orange_mango)
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('Tomato', 'Potato', 'Cabbage','Onion', 'Carrot')
+fruits_and_vegetables = fruits + vegetables
+print(fruits_and_vegetables)
